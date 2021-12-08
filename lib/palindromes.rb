6cf6631252e2e512a84f6f6f03f58858d346f4c8
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-08 14:08:19 -0800

    pass third test for pal_checker
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

--- lib/palindromes.rb
+++ lib/palindromes.rb
@@ -5,6 +5,15 @@ class Text
     @text = input
   end
   def pal_checker
-    return false
+    just_letters = @text.gsub(/[^a-z0-9]/,'')
+    i = 0
+    while (i < (just_letters.length/2).floor)
+      if just_letters[i] == just_letters[just_letters.length-i-1]
+        i += 1
+      else
+        return false
+      end
+    end
+    true
   end
 end
--- spec/palindromes_spec.rb
+++ spec/palindromes_spec.rb
@@ -8,6 +8,12 @@ describe ("#pal_checker") do
   end
   it("returns true if the text is a one-word palindrome") do
     text = Text.new("kayak")
-    expect(text.pal_checker()).to(eq(false))
+    expect(text.pal_checker()).to(eq(true))
+  end
+  it("returns true if the text is a multi-word palindrome") do
+    text = Text.new("a man, a plan, a canal, panama")
+    expect(text.pal_checker()).to(eq(true))
   end
 end
+
+# teh a man, a plan, a one c ewoanal, panama fin
