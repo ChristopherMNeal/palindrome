04f6ac0bc5132aa17cb0df96cf3e8a6c8a5d8142
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-08 15:36:30 -0800

    WIP: add pal_counter()
    Co-authored-by: Drew Henderson <drewhendersonemail@gmail.com>

--- lib/palindromes.rb
+++ lib/palindromes.rb
@@ -3,12 +3,12 @@ require('pry')
 class Text
   def initialize(input)
     @text = input
+    
   end
-  def pal_checker
-    just_letters = @text.gsub(/[^a-z0-9]/,'')
+  def pal_checker(txt)   
     i = 0
-    while (i < (just_letters.length/2).floor)
-      if just_letters[i] == just_letters[just_letters.length-i-1]
+    while (i < (txt.length/2).floor)
+      if txt[i] == txt[txt.length-i-1]
         i += 1
       else
         return false
@@ -16,4 +16,30 @@ class Text
     end
     true
   end
+
+  def pal_counter()
+    #amanaplanacanalpanama
+   # "a man, a plan, a canal, panama"
+    just_letters = @text.gsub(/[^a-z0-9]/,'').downcase
+    count = 0
+    for i in 0..just_letters.length-1 do
+      j=i+1
+      while j < just_letters.length-1 do
+        if just_letters[i] == just_letters[j]
+          pal_check_slice = just_letters[i, j]
+          # binding.pry
+          if(pal_checker(pal_check_slice))
+            count+=1
+            # binding.pry
+          end
+        end
+        j+=1
+      end
+    end
+    count
+  end
 end
+
+
+#I WANT A KAYAK, A TAKAY,I OKAY?
+#KAYAK KAYAK KAYAK
--- spec/palindromes_spec.rb
+++ spec/palindromes_spec.rb
@@ -2,17 +2,17 @@ require('rspec')
 require('palindromes.rb')
 
 describe ("#pal_checker") do
-  it("returns false if the text is not a palindrome") do
-    text = Text.new("this is not a palindrome")
-    expect(text.pal_checker()).to(eq(false))
-  end
-  it("returns true if the text is a one-word palindrome") do
-    text = Text.new("kayak")
-    expect(text.pal_checker()).to(eq(true))
-  end
+  # it("returns false if the text is not a palindrome") do
+  #   text = Text.new("this is not a palindrome")
+  #   expect(text.pal_counter()).to(eq(false))
+  # end
+  # it("returns true if the text is a one-word palindrome") do
+  #   text = Text.new("kayak")
+  #   expect(text.pal_counter()).to(eq(true))
+  # end
   it("returns true if the text is a multi-word palindrome") do
     text = Text.new("a man, a plan, a canal, panama")
-    expect(text.pal_checker()).to(eq(true))
+    expect(text.pal_counter()).to(eq(true))
   end
 end
 
